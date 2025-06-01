c976901055d5424d2ef32278b5982d7df2e69852
saimsaeed <saimsaeed640@gmai.com>
2025-06-01 17:36:01 +0000

    updated

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // Create a default axios instance
 const axiosInstance = axios.create({
   baseURL: "https://payroll-backend-phi.vercel.app/", // Replace with your API base URL
-  timeout: 10000, // Optional: Set a timeout for requests
   headers: {
     "Content-Type": "application/json", // Default content type
   },
